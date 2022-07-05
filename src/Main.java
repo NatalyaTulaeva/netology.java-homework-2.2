7654d50fa8930260feb46f088d8c3e6a66c7d97d
NatalyaTulaeva <n.tulaeva1@gmail.com>
2022-07-05 18:33:09 +0500

    java qa 2.2.2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        
+        int startingScore = 1;
+        int replanAmount = 10_000_000;
+        int accruedBonuses;
+        int finalScore;
+
+        if (replanAmount > 1000) {
+            accruedBonuses = replanAmount / 100;
+            System.out.println("Сумма начисленных бонусов:" + accruedBonuses);
+
+        } else {
+            accruedBonuses = 0;
+            System.out.println("Сумма начисленных бонусов = 0");
+        }
+        finalScore = replanAmount + startingScore + accruedBonuses;
+        System.out.println(("Ваш итоговый счет: ") + finalScore);
     }
+
 }
